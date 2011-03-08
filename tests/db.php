afa1e17e3885b51edf9f1ab7b4daa64ea48f3bb7
Brian J Brennan <brianloveswords@gmail.com>
2011-03-07 19:31:03 -0500

    Setup was misguided.

---
+++ tests/db.php
@@ -0,0 +1,6 @@
+<?php
+require_once('simpletest/autorun.php');
+require_once('../classes/db.php');
+
+class TestOfDb extends UnitTestCase {
+}
--- tests/setup.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once('simpletest/autorun.php');
-require_once('../classes/setup.php');
-
-class TestOfSetup extends UnitTestCase {
-
-}
