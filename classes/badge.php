fa4e7f92bec82fa340abf35f57e9323f28dfde6f
Brian J Brennan <brianloveswords@gmail.com>
2011-03-08 00:08:47 -0500

    Badges can be saved

--- classes/badge.php
+++ classes/badge.php
@@ -1,22 +1,61 @@
 <?php
+/* created with:
+  
+  CREATE TABLE badge (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      uuid VARCHAR(255) NOT NULL UNIQUE,
+      name VARCHAR(255) NOT NULL,
+      description VARCHAR(255) NOT NULL,
+      image VARCHAR(255) NOT NULL,
+      validation VARCHAR(255) NOT NULL,
+      created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      ipaddr VARCHAR(255) NOT NULL
+    );
+ */
 require_once('db.php');
 class BadgeError extends Exception {}
 class Badge {
   private $data;
-  private $dirty = FALSE;
-  private function __construct($data){
+  private $dirty = TRUE;
+  private function __construct($data, $new = TRUE){
     $this->data = $data;
   }
+  
   public function __get($name) {
     return $this->data[$name];
   }
-  /*
+  public function isSaved() {
+    return !($this->dirty);
+  }
+  public function save() {
+    $table = self::db()->useTable('badge');
+    
+    $this->data['ipaddr'] = $_SERVER['HTTP_HOST'];
+    $this->data['uuid'] = $this->uuid();
+    if (($id = $table->insert($this->data)) != FALSE) {
+      $this->data['id'] = $id;
+      $this->dirty = FALSE;
+      return TRUE;
+    } else {
+      return FALSE;
+    }
+  }
+  private function uuid() {
+    // hash by name, ipaddress, and image
+    $d = $this->data;
+    return hash('sha1', $d['name'] . $d['ipaddr'] . $d['image']);
+  }
   public function __set($name, $value) {
     $this->dirty = TRUE;
     return $this->data[$name] = $value;
   }
-  */
-  public static function create($data) {
+  
+  private static $db;
+  private static function db() {
+    if (empty(self::$db)) self::$db = new Db();
+    return self::$db;
+  }
+  public static function create($data = NULL) {
     if (!is_array($data)) {
       throw new BadgeError('Badge::create(): parameter must be an associative array');
     }
--- tests/badge.php
+++ tests/badge.php
@@ -4,23 +4,35 @@ require_once('../classes/badge.php');
 
 class TestOfBadge extends UnitTestCase {
   function testCreatingNewBadge() {
-    $data = array(
-      'name' => 'Badge of Awesome',
-      'description' => 'For being totally rad',
-      'validation' => 'http://google.com',
-      'image' => 'now this one is tricky...'
-    );
-    
+    $data = $this->generateBadgeData();
     $badge = Badge::create($data);
     $this->assertTrue($badge instanceof Badge);
     $this->assertEqual($badge->name, 'Badge of Awesome');
     
     // don't create badge without data
     try {
-      $badge2 = Badge::create('nahh');
+      $badge2 = Badge::create();
       $this->assertTrue(FALSE); // fail, should not create badge;
     } catch (BadgeError $e) {
       $this->assertTrue(TRUE); // success;
     }
+  }
+  function testSavingNewBadge() {
+    $data = $this->generateBadgeData();
+    $badge = Badge::create($data);
+    $this->assertFalse($badge->isSaved());
+    $badge->save();
+    $this->assertTrue($badge->isSaved());
+    $id = $badge->id;
+    $this->assertFalse(empty($id));
+  }
+
+  private function generateBadgeData() {
+    return array(
+      'name' => 'Badge of Awesome',
+      'description' => 'For being totally rad',
+      'validation' => 'http://google.com',
+      'image' => 'now this one is tricky...'
+    );
   }
 }
