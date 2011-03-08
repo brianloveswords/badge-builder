abe0d3d61eedda9409473f3a02c21b7cf3a28823
Brian J Brennan <brianloveswords@gmail.com>
2011-03-07 19:56:29 -0500

    First database test.

--- classes/db.php
+++ classes/db.php
@@ -1,6 +1,19 @@
 <?php
 require_once('../settings.php');
 class Db {
-
-  
+  public $handle;
+  public function __construct($settings = null) {
+    if (empty($settings)) global $settings;
+    $this->settings = $settings['db'];
+    $this->connect();
+  }
+  public function connectionValid() {
+    if (empty($this->handle)) return false;
+    else return true;
+  }
+  public function connect() {
+    $_db = $this->settings;
+    $h = @mysql_connect($_db['host'], $_db['user'], $_db['password']);
+    return ($this->handle = $h);
+  }
 }
--- tests/db.php
+++ tests/db.php
@@ -3,4 +3,13 @@ require_once('simpletest/autorun.php');
 require_once('../classes/db.php');
 
 class TestOfDb extends UnitTestCase {
+  /**
+   * Assumes valid settings.php file!
+   */
+  function testDatabaseConnection() {
+    $db = new Db();
+    $this->assertTrue($db->connectionValid());
+    $db2 = new Db(array('db'=>array()));
+    $this->assertFalse($db2->connectionValid());
+  }
 }
