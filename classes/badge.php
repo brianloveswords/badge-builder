3d3dde211811ab53490b23d13488c6e86ac54924
Brian J Brennan <brianloveswords@gmail.com>
2011-03-07 23:08:22 -0500

    Badge::create() does some error checking.

--- classes/badge.php
+++ classes/badge.php
@@ -1,14 +1,25 @@
 <?php
 require_once('db.php');
+class BadgeError extends Exception {}
 class Badge {
   private $data;
+  private $dirty = FALSE;
   private function __construct($data){
     $this->data = $data;
   }
   public function __get($name) {
     return $this->data[$name];
   }
+  /*
+  public function __set($name, $value) {
+    $this->dirty = TRUE;
+    return $this->data[$name] = $value;
+  }
+  */
   public static function create($data) {
+    if (!is_array($data)) {
+      throw new BadgeError('Badge::create(): parameter must be an associative array');
+    }
     $c = __CLASS__;
     return new $c($data);
   }
--- tests/badge.php
+++ tests/badge.php
@@ -14,5 +14,13 @@ class TestOfBadge extends UnitTestCase {
     $badge = Badge::create($data);
     $this->assertTrue($badge instanceof Badge);
     $this->assertEqual($badge->name, 'Badge of Awesome');
+    
+    // don't create badge without data
+    try {
+      $badge2 = Badge::create('nahh');
+      $this->assertTrue(FALSE); // fail, should not create badge;
+    } catch (BadgeError $e) {
+      $this->assertTrue(TRUE); // success;
+    }
   }
 }
