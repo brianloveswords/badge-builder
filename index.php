55adc3a0e22353e8f0a89d7998b3f1171c72d3cd
Brian J Brennan <brianloveswords@gmail.com>
2011-03-09 16:56:10 -0500

    Add description, disclaimer.

--- index.php
+++ index.php
@@ -8,13 +8,22 @@
     <div role='container'>
       <h1>Badge Builder</h1>
       <p role='description'>
-        Welcome to the badge builder! This is a prototype
+        Welcome to the Badge Builder prototype! The Badge Builder allows you
+        to experiment with badges and make one of your own for your own
+        website or to give to someone else. Badges can be used to signal
+        achievements, skills, qualities or affiliations.
       </p>
       <h2>You can...</h2>
       <ul>
         <li><a href="build/">Build a badge</a></li>
         <li><a href="view/">View all badges</a></li>
       </ul>
+      <hr>
+      <p role='description'>
+        <strong>Note</strong> that this is a prototype: all badges are public,
+        and things may break or go away. That said, we'll do our best to make
+        sure things don't break.
+      </p>
    </div>
   </body>
 </html>
