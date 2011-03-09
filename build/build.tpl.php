cec5a99116094c66a8764c1ef8ee4ff342c8844e
Brian J Brennan <brianloveswords@gmail.com>
2011-03-09 16:55:59 -0500

    Move the main description to the index.

--- build/build.tpl.php
+++ build/build.tpl.php
@@ -7,12 +7,6 @@
   <body>
     <div role='container'>
       <h1>Badge Builder</h1>
-      <p role='description'>
-        Badges can be used to signal achievements, skills, qualities or
-        affiliations. The Badge Builder allows you to experiment with badges
-        and make one of your own for your own website or to give to someone
-        else.
-      </p>
       <form action="#" enctype="multipart/form-data" method="post" name="builder">
         <div>
           <label>
