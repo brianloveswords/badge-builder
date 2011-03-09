72c4d8b3cd33d84c16442252902b4edb74373194
Brian J Brennan <brianloveswords@gmail.com>
2011-03-09 17:32:14 -0500

    Maintenance page.

---
+++ maintenance.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Badge Builder</title>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+  </head>
+  <body>
+    <div role='container'>
+      <h1>Badge Builder</h1>
+      <p role='description'>
+        Down for maintenance! Check back in a few minutes.
+      </p>
+   </div>
+  </body>
+</html>
